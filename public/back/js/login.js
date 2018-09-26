7e0c686331b49ceaa67da1ace899864bb26e065d
xiao <1195082095@qq.com>
2018-09-26 19:37:40 +0800

    静态表单验证完成

---
+++ public/back/js/login.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Administrator on 2018/9/26.
+ */
+$(function () {
+  $("#form").bootstrapValidator({
+    feedbackIcons: {/*input状态样式图片*/
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields:{
+      username:{
+        validators:{
+          notEmpty:{
+            message:"用户名不能为空"
+          },
+          stringLength:{
+            min:2,
+            max:6,
+            message:"用户名不能少于2-6位"
+          }
+        }
+      },
+      password:{
+        validators:{
+          notEmpty:{
+            message:"密码不能为空"
+          },
+          stringLength:{
+            min:6,
+            max:12,
+            message:"用户名不能少于6-12位"
+          }
+        }
+      }
+    }
+  });
+  
+})
