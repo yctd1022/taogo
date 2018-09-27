4313a47d62de2ed4bd72c57c6ef5309fc4f051e0
xiao <1195082095@qq.com>
2018-09-27 19:32:06 +0800

    pc公共部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -6,4 +6,42 @@ $(document).ajaxStart(function () {
 });
 $(document).ajaxStop(function () {
   NProgress.done();
+});
+$(function () {
+  //下拉菜单栏
+  $(".nav li .category").click(function () {
+    $(".nav li .child").stop().slideToggle();
+  });
+  $(".tg_topbar .icon_menu").click(function () {
+    $(".tg_aside").toggleClass("hidemenu");
+    $(".tg_mian").toggleClass("hidemenu");
+    $(".tg_mian .tg_topbar").toggleClass("hidemenu");
+  });
+  $(".tg_topbar .icon_logout").click(function () {
+    $("#loginout_modal").modal('show');
+  });
+  $("#loginout").click(function () {
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      success: function (info) {
+        //console.log(info);
+        if(info.success){
+          location.href = "login.html";
+        }
+      }
+    })
+  });
+  $.ajax({
+    type: "get",
+    url: "/employee/checkRootLogin",
+    dataType: "json",
+    success: function (info) {
+      console.log(info);
+      if(info.error === 400){
+        location.href = "login.html"
+      }
+    }
+  })
 })
