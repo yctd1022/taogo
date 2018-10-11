517d1580b08485ab49dd44d30ad79119c854b40c
xiao <1195082095@qq.com>
2018-10-11 22:14:51 +0800

    用户中心完成提交

--- public/front/js/user.js
+++ public/front/js/user.js
@@ -1,3 +1,30 @@
 /**
  * Created by Administrator on 2018/10/11.
  */
+$(function () {
+  $.ajax({
+    type: "get",
+    url: "/user/queryUserMessage",
+    dataType: "json",
+    success: function (info) {
+      console.log(info)
+      if(info.error == 400){
+        location.href = "login.html";
+        return;
+      }
+      $("#userInfo").html(template("user_Tmp",info));
+    }
+  });
+  $("#logout").on("click", function () {
+    $.ajax({
+      type: "get",
+      url: "/user/logout",
+      dataType: "json",
+      success: function (info) {
+        if ( info.success ) {
+          location.href = "login.html";
+        }
+      }
+    })
+  })
+})
