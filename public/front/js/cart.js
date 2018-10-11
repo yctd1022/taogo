3d4bcbdd741b5d0bc2f993a487d68e45b9a9b1d7
xiao <1195082095@qq.com>
2018-10-12 02:06:14 +0800

    购物车部分完成

--- public/front/js/cart.js
+++ public/front/js/cart.js
@@ -1,3 +1,57 @@
 /**
  * Created by Administrator on 2018/10/11.
  */
+$(function () {
+  render();
+  function render() {
+    setTimeout(function () {
+      $.ajax({
+        type: "get",
+        url: "/cart/queryCart",
+        dataType: "json",
+        success: function (info) {
+          console.log(info);
+          if(info.error === 400){
+            location.href = "login.html?retUrl=" + location.href;
+            return;
+          }
+          $(".mui-table-view").html(template("cart_Tmp",{arr:info}));
+          mui(".mui-scroll-wrapper").pullRefresh().endPulldownToRefresh();
+        }
+      })
+    },500)
+  };
+  mui.init({
+    pullRefresh : {
+      container:".mui-scroll-wrapper",//下拉刷新容器标识
+      down : {
+        auto: true, // 加载自动下拉刷新一次
+        callback: function() {
+          console.log( "发送ajax请求, 进行页面刷新" )
+          render();
+        }
+      }
+    }
+  });
+  $(".mui-table-view").on("tap",".btn_delete", function () {
+    var id = $(this).data("id");
+    $.ajax({
+      type: "get",
+      url: "/cart/deleteCart",
+      data: {
+        id: [id]
+      },
+      dataType: "json",
+      success: function (info) {
+        if(info.success){
+          render();
+        }
+      }
+    })
+  });
+  $('.lt_main').on("tap", ".btn_edit", function () {
+    var obj = this.dataset;
+    var id = obj.id;
+    console.log(obj);
+  })
+})
